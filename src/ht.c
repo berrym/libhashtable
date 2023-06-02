5a6d94c544666ab671c53c6ba39b9f9519b03649
Michael Berry <trismegustis@gmail.com>
2023-06-02 12:00:18 -0400

    Make sure to free bucket key val pair when removing a chain in ht_remove.

--- src/ht.c
+++ src/ht.c
@@ -341,6 +341,10 @@ void ht_remove(ht_t *ht, void *key)
             ht->buckets[idx].key = ht->callbacks.key_copy(cur->key);
             ht->buckets[idx].val = ht->callbacks.val_copy(cur->val);
             ht->buckets[idx].next = cur->next;
+            ht->callbacks.key_free(cur->key);
+            ht->callbacks.val_free(cur->val);
+            cur->key = NULL;
+            cur->val = NULL;
             free(cur);
             cur = NULL;
         }
@@ -357,6 +361,8 @@ void ht_remove(ht_t *ht, void *key)
             prev->next = cur->next;
             ht->callbacks.key_free(cur->key);
             ht->callbacks.val_free(cur->val);
+            cur->key = NULL;
+            cur->val = NULL;
             free(cur);
             cur = NULL;
             ht->length--;
